dfbcd8adf2d8f5f843eeacb74d4043c4a6273566
Yvens Almeida <yvensalmeida21@gmail.com>
2024-06-14 00:32:07 -0300

    Aprendendo a usar as props em react

--- exercicio02/src/App.jsx
+++ exercicio02/src/App.jsx
@@ -1,12 +1,17 @@
 import Card from './components/Card/Card'
+import theBoys from './assets/boys.jpg'
+import invencivel from './assets/invencivel.jpg'
 import './styles/global.css'
 
-function App(){
-  return(
+function App() {
+  return (
     <div>
-      <Card />
-      <Card />
-      <Card />
+      <Card titulo="The Boys (2019)" poster={theBoys} link = "https://www.primevideo.com/detail/0SSV1CIYHPUKDFUE5JOYRDKF06/ref=atv_hm_hom_c_cjm7wb_2_1?jic=8%7CEgNhbGw%3D"/>
+
+      <Card titulo="Invencível (2021)" poster={invencivel} link ="https://www.primevideo.com/detail/0KRK7IABRYDORGPDYP1XO3OT0Q/ref=atv_hm_hom_c_cjm7wb_2_3?jic=8%7CEgNhbGw%3D"/>
+
+
+      <Card titulo="Nova serie" />
     </div>
   )
 }
--- exercicio02/src/components/Card/Card.jsx
+++ exercicio02/src/components/Card/Card.jsx
@@ -1,21 +1,20 @@
-import './Card.css'
-import poster from '../../assets/boys.jpg';
+import "./Card.css";
+import poster from "../../assets/boys.jpg";
 
-function Card(){
-    return(
+function Card({titulo, poster, link}) {
+    return (
         <div>
             <div className="container">
-                <img className='poster' src={poster} alt="The boys poster" title='The boys poster' width={300}/>
+                <img className="poster" src={poster} alt="The boys poster" title="The boys poster" width={300}/>
 
                 <div className="conteudo">
-                    <h2>The Boys (2019)</h2>
+                    <h2>{titulo}</h2>
                     <p>The Boys se passa em um universo onde indivíduos superpoderosos são reconhecidos como heróis pelo público em geral e pertencem à poderosa corporação Vought International, que os comercializa e monetiza. Fora de suas personas heroicas, a maioria é arrogante e corrupta. A série se concentra principalmente em dois grupos: os Sete, o principal time de super-heróis da Vought International, e os Rapazes / The Boys, vigilantes, que procuram manter os heróis corrompidos sob controle.</p>
-                    <button>Assistir agora</button>
+                    <a href={link}><button>Assistir agora</button></a>
                 </div>
             </div>
-            
         </div>
-    )
+    );
 }
 
 export default Card;
