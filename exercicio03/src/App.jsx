5cd65868a439b499c9c57b14b2640172bb535086
Yvens Almeida <yvensalmeida21@gmail.com>
2024-06-14 01:50:25 -0300

    Alterando algumas coisas

--- exercicio03/src/App.jsx
+++ exercicio03/src/App.jsx
@@ -5,7 +5,15 @@ import './styles/global.css'
 function App(){
   return(
     <div>
-        <Card avatar = {avatar} nome = "Yvens Almeida" bio = "Front-end developer | Computer Science - UFC" email = "Yvensalmeida21@gmail.com" telefone = "+55 (12)9.1234-5678" githubUrl = "https://github.com/Yvenscivel" linkedinUrl = "https://www.linkedin.com/in/yvens-almeida/" instagramUrl = "https://www.instagram.com/yvens_almeida/"/>
+        <Card 
+          avatar = {avatar} 
+          nome = "Yvens Almeida" 
+          bio = "Front-end developer | Computer Science - UFC" 
+          email = "Yvensalmeida21@gmail.com" telefone = "+55 (12)9.1234-5678" 
+          githubUrl = "https://github.com/Yvenscivel" 
+          linkedinUrl = "https://www.linkedin.com/in/yvens-almeida/" 
+          instagramUrl = "https://www.instagram.com/yvens_almeida/"
+        />
     </div>
   )
 }
